31f61b091105925d3af252aa1759ec8fd7f8fd5e
Ganesh Rao <ndlganeswararao@gmail.com>
2018-10-02 22:21:21 +0530

    Intial Project files

---
+++ src/components/Hello.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+export interface IProps{
+    name: string;
+    enthusiasmLevel?: number;
+}
+
+export interface IState{
+    currentEnthusiasmLevel: number;
+}
+
+class Hello extends React.Component<IProps, IState> {
+    constructor(props: IProps){
+        super(props);
+        this.state = {
+            currentEnthusiasmLevel: props.enthusiasmLevel || 1
+        }
+    }
+    onDecrement = () => {this.updateEnthusisasm(this.state.currentEnthusiasmLevel - 1)}
+    onIncrement = () => {this.updateEnthusisasm(this.state.currentEnthusiasmLevel + 1)}
+    
+    render(){
+const {name, enthusiasmLevel} = this.props;
+        if(enthusiasmLevel < 0 ){
+            throw new Error('You could be a little more enthusiastic :D');
+            
+        }
+        return(<div>
+            Hello Component.....{name + getExclaimationMarks(this.state.currentEnthusiasmLevel)}
+        <button onClick={() => this.onDecrement()}>-</button>
+        <button onClick={() =>this.onIncrement()}>+</button>
+        </div>);
+    }
+    updateEnthusisasm(currentEnthusiasmLevel: number){
+        this.setState({currentEnthusiasmLevel});
+    }
+    
+}
+
+export function getExclaimationMarks(noOfTimes:number){
+    return noOfTimes > 0 ? Array(noOfTimes + 1).join('!'):'!';
+}
+export default Hello
